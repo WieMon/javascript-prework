093e5d3f13c813953ab48ac0e6b98abed0a038aa
Monika Wieczorek <monikawieczoreq@gmail.com>
2019-11-01 15:17:41 +0100

    Add computerMove and playerMove to script.js Call printMessage in script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+printMessage('Zagrałam ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywam!');
