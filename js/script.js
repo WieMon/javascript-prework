733c394c6a5dc11fd6f48551814479b5264b31a8
Monika Wieczorek <monikawieczoreq@gmail.com>
2019-11-02 22:34:01 +0100

    Add function getMoveName

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    } else if(argMoveId == 2){
+      return 'papier';
+    } else if(argMoveId == 3){
+      return 'nożyce';
+    } else{
+      return 'nieznany ruch';
+    }
+  }
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if (randomNumber == 2){
-    computerMove = 'papier';
-} else if (randomNumber == 3){
-    computerMove = 'nożyce';
-}
 printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if (playerInput == '2'){
-    playerMove = 'papier';
-} else if (playerInput == '3'){
-    playerMove = 'nożyce';
-}
+let playerMove = getMoveName(playerInput);
 
 printMessage('Twój ruch to: ' + playerMove);
 
-if(computerMove == playerMove){
+console.log('moves:', computerMove, playerMove)
+
+function displayResult(computerMove, playerMove){
+} if(computerMove == playerMove){
     printMessage('Remis!');
 } else if (computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
@@ -46,3 +46,5 @@ if(computerMove == playerMove){
 } else {
     printMessage('Ups - wybierz swój ruch');
 }
+
+displayResult (computerMove, playerMove)
